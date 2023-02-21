b2bddc8f23fe5727ed690a3c4614ff8a0dd72e39
Aman Singh <haaamansingh007@gmail.com>
2023-02-22 02:27:57 +0530

    Updated

--- components/Overview.jsx
+++ components/Overview.jsx
@@ -3,27 +3,28 @@ import Link from "next/link";
 export default function Overview(params) {
   return (
     <>
-      <div className="container my-3 my-lg-5 ">
+      <div className="container my-3 my-lg-5 bg-grey">
         <div className="row display-1 text-poppins text-uppercase">
-          <div className="col-md-3">listen</div>
-          <div className="col-md-3"></div>
-          <div className="col-md-3">new</div>
-          <div className="col-md-3"></div>
-          <div className="col-md-4">podcast</div>
-          <div className="col-md-4"></div>
-          <div className="col-md-4 text-md-end">order</div>
+          <div className="col-sm text-start">listen</div>
+          <div className="col-sm-4 d-lg-block d-none pic1 rounded-pill my-3 shadow"></div>
+          <div className="col-sm">new</div>
+          <div className="col-sm-2 d-lg-block d-none pic2 rounded-pill my-3 shadow"></div>
+          <div className="col-sm text-start">podcast</div>
+          <div className="col-sm-3 d-lg-block d-none pic2 rounded-pill my-3 shadow"></div>
+          <div className="col-sm">order</div>
         </div>
-        <div className="row my-4 text-dm">
+
+        <div className="row my-5 text-dm">
           <div className="col-md-6">
             <p className="opacity-50 pe-3 fs-6">
               We are a team of writers and bloggers who are passionate about
-              sharing our thoughts and ideas with the world. Our Mission Our
-              mission is to provide high-quality content that informs, educates,
-              and inspires our readers.
+              sharing our thoughts and ideas with the world. Our mission is to
+              provide high-quality content that informs, educates, and inspires
+              our readers.
             </p>
             <div className="col-lg-4 d-grid">
               <Link href="/contact" className="d-grid">
-                <button className="btn btn-outline-light btn-sm my-3 py-2 rounded">
+                <button className="btn btn-outline-light btn-sm my-3 py-2 rounded shadow-sm">
                   Get Started
                   <i
                     class="fa-solid fa-arrow-up-long ms-2"
