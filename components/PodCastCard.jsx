8d02f5ce61ad8c3a19df360f63a528b61ff32344
Aman Singh <haaamansingh007@gmail.com>
2023-02-28 20:40:42 +0530

    Updated

--- components/PodCastCard.jsx
+++ components/PodCastCard.jsx
@@ -2,7 +2,7 @@ export default function PodCastCard(props) {
   return (
     <>
       <div className={`col-md-${props.col}`}>
-        <div className="card card-cover btn-shrink h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg image bg-grad">
+        <div className="card card-cover btn-shrink h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg image border-0 bg-grad">
           <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-3">
             <h2 className="pt-5 mt-auto mb-2 lh-1 fw-bold">{props.title}</h2>
             <ul className="d-flex list-unstyled">
--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,17 @@
 export default function Contact(params) {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="row my-5 py-4">
+          <div className="col-md-5">
+            <p className="fs-1 mb-4">
+              Do you have questions, feedback, or suggestions?
+            </p>
+            <p className="fs-1 mb-4">We&apos;d love to hear from you.</p>
+          </div>
+          <div className="col-md-7"></div>
+        </div>
+      </div>
+    </>
+  );
 }
