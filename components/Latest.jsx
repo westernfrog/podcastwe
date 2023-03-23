3f0a70d70fcb49ae72f3d50ea6c75d7596aeb055
Aman Singh <haaamansingh007@gmail.com>
2023-03-23 14:52:31 +0530

    Updated

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -1,4 +1,4 @@
-import PodCastCard from "./PodCastCard";
+import LatestCard from "./LatestCard";
 
 export default function Latest(props) {
   return (
@@ -6,14 +6,13 @@ export default function Latest(props) {
       <div className="container mb-5">
         <div className="row align-items-stretch g-4 py-3">
           {props.posts.map((post, index) => (
-            <PodCastCard
+            <LatestCard
               key={index}
               id={post.frontmatter.title}
               col={6}
               alt={"Latest"}
               url={post.slug}
               title={post.frontmatter.title}
-              desc={post.frontmatter.excerpt}
             />
           ))}
         </div>
---
+++ components/LatestCard.jsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+import { Card, Col, Grid, Text, Loading } from "@nextui-org/react";
+import { useRandomPodcastPhoto } from "./GetRandomPhotos";
+
+export default function LatestCard(props) {
+  const { photoUrl } = useRandomPodcastPhoto({ id: props.id });
+
+  if (!photoUrl) {
+    return (
+      <Grid sm={6} md={props.col} className="g-4">
+        <Link href={`/blog/${props.url}`} className="w-100">
+          <Card
+            isPressable
+            isHoverable
+            css={{ h: "300px" }}
+            style={{ backgroundColor: "#ffffff20" }}
+          >
+            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+              <Col>
+                <Text
+                  size={12}
+                  weight="bold"
+                  transform="uppercase"
+                  color="#9E9E9E"
+                >
+                  New
+                </Text>
+                <Text h3 color="white" className="text-shadow-3">
+                  {props.title}
+                </Text>
+              </Col>
+            </Card.Header>
+            <Card.Body css={{ p: 0 }}>
+              <Loading
+                className="my-auto"
+                type="points-opacity"
+                color="white"
+                size="xl"
+              />
+            </Card.Body>
+          </Card>
+        </Link>
+      </Grid>
+    );
+  }
+
+  return (
+    <>
+      <Grid sm={6} md={props.col} className="g-4">
+        <Link href={`/blog/${props.url}`}>
+          <Card
+            isHoverable
+            isPressable
+            css={{ w: "100%", h: "300px" }}
+            style={{ backgroundColor: "inherit" }}
+          >
+            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+              <Col>
+                <Text
+                  size={12}
+                  weight="bold"
+                  transform="uppercase"
+                  color="#9E9E9E"
+                >
+                  {props.alt}
+                </Text>
+                <Text h3 color="white" className="text-shadow-3">
+                  {props.title}
+                </Text>
+              </Col>
+            </Card.Header>
+            <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                src={photoUrl}
+                objectFit="cover"
+                width="100%"
+                height="100%"
+                alt="Podcast app background"
+              />
+            </Card.Body>
+          </Card>
+        </Link>
+      </Grid>
+    </>
+  );
+}
